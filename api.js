f5a5b0c78940d8a53bd3f8744f0d0fcce09cad70
nicholasnovakovic <nova@DESKTOP-TBQP42M.localdomain>
2020-05-09 14:29:17 +0800

    prepared api template

---
+++ api.js
@@ -0,0 +1,46 @@
+var control = require('./controllers/controller');
+
+const authenticate = () => {
+
+};
+
+/*
+For all the restaurant owners
+*/
+const addItem = () => {
+
+};
+
+const editItem = () => {
+
+};
+
+const deleteItem = () => {
+
+};
+
+const getOrder = () => {
+
+};
+
+/*
+HCW user methods
+*/
+const viewItems = () => {
+
+};
+
+const orderItem = () => {
+
+};
+
+/*
+Donator methods
+*/
+const viewOrganisations = () => {
+
+};
+
+const selectOrganisation = () => {
+
+};
