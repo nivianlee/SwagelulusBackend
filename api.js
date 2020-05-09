c4a32b8e569cf01258ad485ee79baa7f82df2550
nicholasnovakovic <nova@DESKTOP-TBQP42M.localdomain>
2020-05-09 18:42:15 +0800

    Added addItems and getOrders

--- api.js
+++ api.js
@@ -4,5 +4,7 @@ var control = require('./controllers/controller');
 
 router.post('/login', control.authenticate);
 router.get('/users', control.getUsers);
+router.post('/additem', control.addItem);
+router.post('/orders', control.getOrders);
 
 module.exports = router;
--- controllers/controller.js
+++ controllers/controller.js
@@ -13,7 +13,9 @@ const authenticate = (request, response) => {
   const id = request.body.id;
   db.pool.query(`SELECT * FROM restaurants WHERE resID = '${id}'`, [], (error, results) => {
     if (error) {
-      throw error;
+      console.log(err);
+      res.status(400).end(JSON.stringify(err));
+      return;
     }
     if (results[0]) {
       response.status(200).json(results[0]);
@@ -27,6 +29,7 @@ const authenticate = (request, response) => {
           console.log('id = user');
         } else {
           console.log('id does not belong to restaurant or user');
+          response.status(200).json({ valid: false })
         }
       });
     }
@@ -36,27 +39,49 @@ const authenticate = (request, response) => {
 /*
 For all the restaurant owners
 */
-const addItem = (req, res) => {
-  db.pool.getConnection((err, connection) => {
-    if (err) {
+
+/**
+ * @param req.body
+ *  @property {string} name
+ *  @property {string} description
+ *  @property {string} image
+ *  @property {number} price
+ *  @property {string} id
+ * @param res
+ *  @property {boolean} success
+ */
+const addItem = (request, response) => {
+  const name = request.body.name;
+  const desc = request.body.description;
+  const image = request.body.image;
+  const price = request.body.price;
+  const id = request.body.id;
+  db.pool.query(`SELECT * FROM restaurants WHERE resID = '${id}'`, [], (error, results) => {
+    if (error) {
       console.log(err);
       res.status(400).end(JSON.stringify(err));
       return;
+    } 
+    if (results[0]) {
+      db.pool.query(`INSERT INTO fooditems (name, description, image, price, resID) \n VALUES ('${name}','${desc}','${image}','${price}','${id}')`, [], (error, results) => {
+        if (error) {
+          console.log(err);
+          res.status(400).end(JSON.stringify(err));
+          return;
+        }
+        var message = "Item successfully added.";
+        response.status(200).end(JSON.stringify(message));
+        console.log(message);
+      });
+    } else {
+      var message = "Sorry you do not own a restaurant.";
+      console.log(message);
+      response.status(200).end(JSON.stringify(message));
     }
-
-    var query = 'SELECT * FROM users';
-    connection.query(query, [], (err, result) => {
-      connection.release();
-      if (err) {
-        console.log(err);
-        res.status(400).end(JSON.stringify(err));
-        return;
-      }
-      res.end(JSON.stringify(result));
-    });
   });
 };
 
+// maybe later mater
 const editItem = (req, res) => {
   db.pool.getConnection((err, connection) => {
     if (err) {
@@ -99,24 +124,27 @@ const deleteItem = (req, res) => {
   });
 };
 
-const getOrder = (req, res) => {
-  db.pool.getConnection((err, connection) => {
-    if (err) {
+/**
+ * @param req.body
+ *  @property {string} id
+ * @param res
+ *  @property {array} orders
+ */
+// resID ==> all foodIDs ==> go through each orderItem for each foodID we check orders
+const getOrders = (request, response) => {
+  const id = request.body.id;
+  db.pool.query(
+    `SELECT * FROM orders 
+    WHERE orderID IN 
+      (SELECT orderID FROM orderitems WHERE foodItemID IN 
+        (SELECT foodItemID FROM fooditems WHERE resID = '${id}'))
+    AND deliveredAt IS NULL`, [], (error, results) => {
+    if (error) {
       console.log(err);
       res.status(400).end(JSON.stringify(err));
       return;
-    }
-
-    var query = 'SELECT * FROM users';
-    connection.query(query, [], (err, result) => {
-      connection.release();
-      if (err) {
-        console.log(err);
-        res.status(400).end(JSON.stringify(err));
-        return;
-      }
-      res.end(JSON.stringify(result));
-    });
+    } 
+    response.status(200).json(results);
   });
 };
 
@@ -227,7 +255,7 @@ module.exports = {
   addItem,
   editItem,
   deleteItem,
-  getOrder,
+  getOrders,
   viewItems,
   orderItem,
   viewOrganisations,
